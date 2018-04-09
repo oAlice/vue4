714e370bd09bfc148774f4e917c2c29755ac131b
aa <aa@qq.com>
2018-04-09 16:29:14 +0800

    Signed-off-by: aa <aa@qq.com>路由后台

--- app.js
+++ app.js
@@ -9,22 +9,40 @@ const pool = mysql.createPool({
 	host:'localhost',
 	user:'root',
 	password:'root',
-	database:'message',
+	database:'house',
 	port:3306
 })
 
 
-app.use('/',(req,res)=>{
+app.use('/sel',(req,res)=>{
 	res.setHeader('Access-Control-Allow-Origin','*');
 	pool.getConnection((err,con)=>{
 		if(err) throw err;
-		con.query(`SELECT * FROM list`,(err,rows)=>{
+		con.query(`SELECT * FROM list WHERE fenlei=${req.body.fenlei}`,(err,rows)=>{
+			console.log(req.body.fenlei)
+			if(err) throw err;
+			res.send(rows);
+		con.release();
+		})
+	})
+})
+
+app.use('/detail',(req,res)=>{
+	res.setHeader('Access-Control-Allow-Origin','*');
+	pool.getConnection((err,con)=>{
+		if(err) throw err;
+		con.query(`SELECT * FROM list WHERE id=${req.body.id}`,(err,rows)=>{
+			// console.log(req.body)
 			if(err) throw err;
 			res.send(rows);
 			con.release();
 		})
 	})
 })
+
+
+
+
 app.listen(3000,()=>{
 	console.log('ok')
 });
